d130443476c01f059772739df30e1d2f83d40b4a
tatyamyam <105143277+tatyamyam@users.noreply.github.com>
2023-08-11 22:27:49 +0900

    kadai_014途中

---
+++ kadai_014用/java_study_2/src/kadai_014/CarExec_Chapter14.java
@@ -0,0 +1,20 @@
+package kadai_014;
+
+public class CarExec_Chapter14 {
+
+	public static void main(String[] args) {
+		// 車クラスのインスタンスを作成する
+		CarExec_Chapter14 car = new CarExec_Chapter14();
+		
+		//車クラスのgearChangeメソッドを実行する
+		car.gearChange(afterGear);
+		//車クラスのrunメソッドを実行する
+		car.run();
+		
+		
+		
+		
+
+	}
+
+}
---
+++ kadai_014用/java_study_2/src/kadai_014/Car_Chapter14.java
@@ -0,0 +1,43 @@
+package kadai_014;
+
+public class Car_Chapter14 {
+	
+	// フィールド
+	private int gear = 1;
+	private int speed = 10;
+	
+	//①車クラスは次のメソッドを持つ
+	public void gearChange(int afterGear) {
+		
+		this.gear = afterGear;
+		
+	}
+	
+	//②車クラスは次のメソッドを持つ
+	public void run() {
+		System.out.println(this.speed);
+		
+	}
+	
+	//ゲッター
+	public int getGear() {
+		return this.gear;	
+	}
+	
+	public int getSpeed() {
+		return this.speed;	
+	}
+	//ここまでがゲッター
+	
+	//セッター
+	public void setGearChange(int afterGear) {
+		this.gear = afterGear;
+		
+	}
+
+	public void setRun() {
+		System.out.println(this.speed);
+		
+	}
+	//ここまでがセッター
+}
