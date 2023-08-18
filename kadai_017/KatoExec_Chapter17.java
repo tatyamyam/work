b9ec476fb53e17255ecb5318bc9145392a94fd97
tatyamyam <105143277+tatyamyam@users.noreply.github.com>
2023-08-18 15:33:43 +0900

    update

--- kadai_017/KatoExec_Chapter17.java
+++ kadai_017/KatoExec_Chapter17.java
@@ -6,7 +6,10 @@ public class KatoExec_Chapter17 {
 		// 実行
 		KatoTaro_Chapter17 taro = new KatoTaro_Chapter17();
 		
+		
+		
 		taro.execIntroduce();
+		System.out.println();
 		
 
 	}
--- kadai_017/Kato_Chapter17.java
+++ kadai_017/Kato_Chapter17.java
@@ -9,17 +9,18 @@ abstract public class Kato_Chapter17 {
 	public String givenName;	//名
 	public String address;		//住所
 	
-	public void commonIntroduce(String familyName, String address) {
-		familyName = "加藤";
-		address = "東京都中野区〇×";
+	
+	public void commonIntroduce() {
 		System.out.println("名前は" + familyName + givenName + "です");
-		System.out.println("住所は" + address + "です");
-	}
-	abstract public void eachIntroduce();	//個別の紹介を出力する
-	
-	public void execIntroduce() {	//実行する
-
+		System.out.println("住所は" + address+ "です");
 	}
 	
+	abstract public void eachIntroduce();	//抽象メソッド
+	
+	public void execIntroduce() {
+		commonIntroduce();
+		eachIntroduce();
+		
+	}
 
 }
