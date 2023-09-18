a4decf048371412795be800be570472e7b4cc23c
tatyamyam <105143277+tatyamyam@users.noreply.github.com>
2023-09-18 21:29:45 +0900

    new update0918

---
+++ tatyamyam/src/test/java/com/example/tatyamyam/TatyamyamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tatyamyam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TatyamyamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
