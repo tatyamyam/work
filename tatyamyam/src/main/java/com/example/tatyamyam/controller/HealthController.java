f498e7b748da83c871a7f05589249d1c010e8586
tatyamyam <105143277+tatyamyam@users.noreply.github.com>
2024-01-03 09:32:05 +0900

    update_240103

--- tatyamyam/src/main/java/com/example/tatyamyam/controller/HealthController.java
+++ tatyamyam/src/main/java/com/example/tatyamyam/controller/HealthController.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.tatyamyam.entity.Account;
 import com.example.tatyamyam.entity.Health;
@@ -12,15 +11,25 @@ import com.example.tatyamyam.security.UserDetailsImpl;
 import com.example.tatyamyam.service.HealthService;
 
 @Controller
-@RequestMapping("/list")
 public class HealthController {
-	private HealthService healthService;
+	private final HealthService healthService;
+	
+	public HealthController(HealthService healthService) {
+		this.healthService = healthService;
+	}
 	
 	@GetMapping("/list")
     public String list(Model model, @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
         Account account = userDetailsImpl.getAccount();
         Health[] healths = healthService.getHealth(account);
+        System.out.println(healths);
         model.addAttribute("healths", healths);
+        System.out.println(healths[2].getWeight());
+        System.out.println(healths[2].getId());
+        System.out.println(healths[3].getId());
+        System.out.println(healths[4].getId());
+        System.out.println(healths[5].getId());
+        System.out.println(healths[6].getId());
         
         return "list";
     }
