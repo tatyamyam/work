61241fe893cb013c0deee79d5b9d20a01c0f7d61
tatyamyam <105143277+tatyamyam@users.noreply.github.com>
2024-09-19 15:27:19 +0900

    new upload_0919

--- spring_tutorial/src/main/java/com/example/springtutorial/controller/AdminUserController.java
+++ spring_tutorial/src/main/java/com/example/springtutorial/controller/AdminUserController.java
@@ -2,7 +2,6 @@ package com.example.springtutorial.controller;
 
 import java.util.List;
 
-import org.springframework.core.Conventions;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,60 +17,58 @@ import com.example.springtutorial.service.UserService;
 @Controller
 public class AdminUserController {
 	private final UserService userService;
-	
+
 	public AdminUserController(UserService userService) {
 		this.userService = userService;
 	}
-	
+
 	@GetMapping("/adminuser")
 	public String adminUser(Model model) {
-		//最新のユーザリストを取得
+		// 最新のユーザリストを取得
 		List<User> users = userService.getAllUsers();
-		
-		//ビューにユーザリストを渡す
+
+		// ビューにユーザリストを渡す
 		model.addAttribute("users", users);
-		
-		//既にインスタンスが存在する場合は行わない
-		if(!model.containsAttribute("userRegisterForm")) {
-			//ビューにフォームクラスのインスタンスを渡す
+
+		// 既にインスタンスが存在する場合は行わない
+		if (!model.containsAttribute("userRegisterForm")) {
+			// ビューにフォームクラスのインスタンスを渡す
 			model.addAttribute("userRegisterForm", new UserRegisterForm());
 		}
-		
+
 		return "adminUserView";
 	}
-	
+
 	@PostMapping("/register")
 	public String registerUser(RedirectAttributes redirectAttributes,
 			@Validated UserRegisterForm form, BindingResult result) {
-		
-		//バリデーションエラーがあったら終了
-		if(result.hasErrors()) {
-			//フォームクラスをビューに受け渡す
-			redirectAttributes.addFlashAttribute("userResisterForm", form);
-			//バリデーション結果をビューに受け渡す
-			redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + Conventions.getVariableName(form), result);
-			
-			//adminuserにリダイレクトしてリストを再表示
+
+		// バリデーションエラーがあったら終了
+		if (result.hasErrors()) {
+			// フォームクラスをビューに受け渡す
+			redirectAttributes.addFlashAttribute("userRegisterForm", form);
+			// バリデーション結果をビューに受け渡す
+			redirectAttributes.addFlashAttribute(BindingResult.MODEL_KEY_PREFIX + "userRegisterForm", result);
+
+			// adminuserにリダイレクトしてリストを再表示
 			return "redirect:/adminuser";
 		}
-			
+
 		try {
-			//リクエストパラメータからのデータを用いてユーザ登録する
+			// リクエストパラメータからのデータを用いてユーザ登録する
 			userService.createUser(form.getUserName(), form.getPassword(), form.getRoleId());
-			
-			//登録成功時は完了メッセージをビューに受け渡す
+
+			// 登録成功時は完了メッセージをビューに受け渡す
 			redirectAttributes.addFlashAttribute("successMessage", "ユーザ登録が完了しました。");
-		} catch(IllegalArgumentException e) {
-			//登録失敗時はエラーメッセージをビューに受け渡す。
+		} catch (IllegalArgumentException e) {
+			// 登録失敗時はエラーメッセージをビューに受け渡す
 			redirectAttributes.addFlashAttribute("failureMessage", e.getMessage());
-			
-			//フォームクラスをビューに受け渡す
+
+			// フォームクラスをビューに受け渡す
 			redirectAttributes.addFlashAttribute("userRegisterForm", form);
-			
 		}
-		
-		//adminuserにリダイレクトしてリストを再表示
+
+		// adminuserにリダイレクトしてリストを再表示
 		return "redirect:/adminuser";
 	}
-
 }
